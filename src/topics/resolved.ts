624e12a797d4e7f64996f1c2f43f473b4e056455
Julia Hou <juahou@gmail.com>
2023-03-01 12:59:30 -0500

    Trailing spaces

--- src/topics/resolved.ts
+++ src/topics/resolved.ts
@@ -4,8 +4,7 @@ export = function (Topics: { setResolved?: (tid: number) => Promise<void>; }) {
     Topics.setResolved = async function (tid: number): Promise<void> {
         let resolved: boolean;
         // 313: These type checks are done because redis saves the object field as a string
-        // vs everything else saves it as a boolean. 
-        
+        // vs everything else saves it as a boolean.
         // The next lines calls a function in a module that has not been updated to TS yet
         /* eslint-disable @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
         if (typeof await db.getObjectField(`topic:${tid}`, 'resolved') === 'string') {
