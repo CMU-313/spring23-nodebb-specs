aff8f184f25a08dc3c7f2ce0765eddc74a808dd4
nikkiakali <nakali@andrew.cmu.edu>
2023-03-02 01:41:49 -0500

    Made final changes t topic.tpl to change resolved sentence UI

--- src/topics/resolved.js
+++ src/topics/resolved.js
@@ -1,23 +1,12 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 const db = require("../database");
 module.exports = function (Topics) {
-    Topics.setResolved = function (tid) {
-        return __awaiter(this, void 0, void 0, function* () {
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-            const resolved = (yield db.getObjectField(`topic:${tid}`, 'resolved')) === 'true';
-            // The next line calls a function in a module that has not been updated to TS yet
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
-            yield db.setObjectField(`topic:${tid}`, 'resolved', !resolved);
-        });
+    Topics.setResolved = async function (tid) {
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        const resolved = await db.getObjectField(`topic:${tid}`, 'resolved') === 'true';
+        // The next line calls a function in a module that has not been updated to TS yet
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+        await db.setObjectField(`topic:${tid}`, 'resolved', !resolved);
     };
 };
