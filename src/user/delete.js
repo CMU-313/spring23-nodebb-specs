09475f9e0eccbcf881706784bb393ff3c979f78c
vasteymnathan <nmvastey@gmail.com>
2023-03-23 15:00:25 -0400

    Added workflow in fly.yml

--- src/user/delete.js
+++ src/user/delete.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const _ = require("lodash");
 const path = require("path");
+const _ = require("lodash");
 const nconf = require("nconf");
 const util = require("util");
 const rimraf = require("rimraf");
