f8d5f456656cbbd9c2b8c634818a283e0b837ce4
kujuo <61703000+kujuo@users.noreply.github.com>
2023-04-10 10:07:51 -0400

    NodeBB changes

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -3,7 +3,7 @@
 const helpers = require('../helpers');
 const user = require('../../user');
 const db = require('../../database');
-
+const request = require('request');
 const Career = module.exports;
 
 Career.register = async (req, res) => {
@@ -19,9 +19,17 @@ Career.register = async (req, res) => {
             num_programming_languages: userData.num_programming_languages,
             num_past_internships: userData.num_past_internships,
         };
+        // What URL should look like: ???/prediction/student1/computer_science/20/m/40/mens_basketball/1/2
+        const domain = "localhost:444"
+        let url = domain + '/' + userCareerData.student_id + '/' + userCareerData.major.replace(/ /g,"_") + '/' + userCareerData.age +  '/' + userCareerData.gender + '/' + userCareerData.gpa.replace('.',"?") + '/' + userCareerData.extra_curricular.replace(/ /g,"_").replace("'","") + '/' + userCareerData.num_programming_languages + '/' + userCareerData.num_past_internships
+        url = url.toLowerCase()
+        await request(url, { json: true }, 
+        (err, response, body) => {
+            
+            userCareerData.prediction = body.good_employee
+        });    
 
-        userCareerData.prediction = Math.round(Math.random()); // TODO: Change this line to do call and retrieve actual candidate success prediction from the model instead of using a random number
-
+        // userCareerData.prediction = Math.round(Math.random()); // TODO: Change this line to do call and retrieve actual candidate success prediction from the model instead of using a random number
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
         res.json({});
